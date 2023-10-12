9c76e43f933aee88711d5c38f4d85cee124e6a89
ideugu <guru@dustinlee.me>
2023-10-12 17:42:42 +0900

    11강 샘플

---
+++ Source/ArenaBattle/Game/ABGameState.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "ABGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API AABGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void HandleBeginPlay() override;
+
+	virtual void OnRep_ReplicatedHasBegunPlay() override;
+};
